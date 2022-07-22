6abb5ce2f1dede5cce74a1bf478a9ca496df4805
Vincenzo Petrone <vipetrone@unisa.it>
2022-07-22 17:06:56 +0200

    [ADD] Adds evaluation callback and R-PPO

--- train/train_ppo.py
+++ train/train_ppo.py
@@ -1,10 +1,10 @@
 import gym
 
 from stable_baselines3 import PPO
+from sb3_contrib import RecurrentPPO
 from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.callbacks import EvalCallback
 import residual_optimization
-from residual_optimization.envs.sine_collision_env import SineCollisionEnv
-from residual_optimization.controllers.admittance_controller_1d import AdmittanceController1D
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime
@@ -13,12 +13,18 @@ import torch as th
 
 # Parallel environments
 env = make_vec_env("SineCollisionEnv-v0", n_envs=4)
-model = PPO("MlpPolicy", env, verbose=1)
+model = RecurrentPPO("MlpLstmPolicy", env, verbose=1)
 
 total_timesteps = 10_000
 print(f'[INFO] Learning for {total_timesteps} timesteps.')
 
-model.learn(total_timesteps=total_timesteps)
+model = PPO("MlpPolicy", env, verbose=1)
+
+eval_callback = EvalCallback(env, best_model_save_path='./logs/',
+                             log_path='./logs/', eval_freq=1,
+                             deterministic=True, render=False)
+
+model.learn(total_timesteps=total_timesteps, callback=eval_callback, eval_freq=1_000)
 
 filepath = os.path.abspath(os.path.dirname(__file__))
 filename = datetime.datetime.now().strftime("ppo_model_%Y_%m_%d-%I:%M:%S_%p")
